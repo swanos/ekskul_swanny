7b2376d4be2f0a1ecc3e1af9cf4afaf48d3fb1a1
SWANos <swanny.aurellia21@gmail.com>
2020-10-31 11:09:05 +0700

    week8

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,7 @@ const app = express();
 app.use(cors({origin: true}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+routes(app);
 
 //week7
 app.get('/index', (req, res) => {

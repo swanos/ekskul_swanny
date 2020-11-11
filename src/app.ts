c9e45a919445535cb8d3ed029a8e3d2f05a2781b
SWANos <swanny.aurellia21@gmail.com>
2020-11-11 13:50:50 +0700

    week9

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import cors from'cors';
 import http from 'http';
 import configData from './environment';
 import routes from './routes';
+import mongoose from 'mongoose';
 
 const app = express();
 
@@ -11,6 +12,21 @@ app.use(cors({origin: true}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 routes(app);
+
+//Connect to database
+mongoose.connect(configData.mongo_uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+})
+const database = mongoose.connection;
+database.once("open", async() => {
+    console.log("Connected to database");
+});
+database.on("error", () => {
+    console.log("Error connecting to database");
+});
 
 //week7
 app.get('/index', (req, res) => {
